115c3c6011423ed6ef08f4a17aa3499dfc85b397
Kamil <xkamilwrobel@gmail.com>
2017-12-06 22:18:15 +0100

    Refactoring

---
+++ db/migrate/20171206210701_products_orders.rb
@@ -0,0 +1,10 @@
+class ProductsOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :products_orders do |t|
+      t.belongs_to :product, index: true
+      t.belongs_to :order, index: true
+      t.integer :quantity
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206204222) do
+ActiveRecord::Schema.define(version: 20171206210701) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20171206204222) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "deleted", default: false
   end
 
   create_table "orders", force: :cascade do |t|
@@ -30,16 +31,6 @@ ActiveRecord::Schema.define(version: 20171206204222) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["customer_id"], name: "index_orders_on_customer_id"
-  end
-
-  create_table "product_orders", force: :cascade do |t|
-    t.integer "product_id"
-    t.integer "order_id"
-    t.integer "quantity"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_product_orders_on_order_id"
-    t.index ["product_id"], name: "index_product_orders_on_product_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -51,6 +42,16 @@ ActiveRecord::Schema.define(version: 20171206204222) do
     t.datetime "updated_at", null: false
     t.boolean "deleted", default: false
     t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  create_table "products_orders", force: :cascade do |t|
+    t.integer "product_id"
+    t.integer "order_id"
+    t.integer "quantity"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_products_orders_on_order_id"
+    t.index ["product_id"], name: "index_products_orders_on_product_id"
   end
 
 end
